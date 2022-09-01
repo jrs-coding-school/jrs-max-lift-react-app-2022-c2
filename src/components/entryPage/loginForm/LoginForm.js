b1bf0eb62d1272c7c1e3dad38a9d283b339032b3
Eric <charlesericvaughn@gmail.com>
2022-09-01 15:29:58 -0400

    added input fields to login & removed link underli

--- src/components/entryPage/loginForm/LoginForm.js
+++ src/components/entryPage/loginForm/LoginForm.js
@@ -3,6 +3,37 @@ import './LoginForm.css'
 
 export default function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <div>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required />
+      <br />
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+      <label>
+        <input type="checkbox"
+          checked="checked"
+          name="remember" /> Remember me
+      </label>
+
+      <br />
+
+      <div>
+        <button type="submit">Login</button>
+
+        <div class="container">
+          <button type="button" class="cancelbtn">Cancel</button>
+          <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
   )
+
+
 }
