54f99f1c45e31676cfcc2546d0c17f5dd0ce56df
Eric <charlesericvaughn@gmail.com>
2022-09-02 12:31:22 -0400

    added a modal to the history data component

--- src/components/historyData/HistoryData.js
+++ src/components/historyData/HistoryData.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useBoolean } from '../../hooks/useBoolean'
 import EditWorkoutModal from '../editWorkoutModal/EditWorkoutModal'
+import Modal from '../Modal/Modal'
 import './HistoryData.css'
 
+
+
 export default function HistoryData() {
+
+  const [isModalOpen, toggleIsModalOpen] = useBoolean(false)
+
+  function handleSubmit() {
+    // do something and then
+    toggleIsModalOpen(); // close after 
+  }
+
+
   return (
     <div>
       HistoryData
@@ -11,7 +24,24 @@ export default function HistoryData() {
       shown in the history page based on which workout is selected or 
       maybe do accordions
       */}
-      <EditWorkoutModal />
-    </div>
+
+      {isModalOpen && (
+        <Modal title="Edit Workout History"
+          closeModal={toggleIsModalOpen} >
+
+          <div>Hi, i'm in the modal now</div>
+
+          <button onClick={toggleIsModalOpen}>
+            Close
+          </button>
+          <button onClick={handleSubmit}>
+            Save Changes
+          </button>
+
+        </Modal>
+      )}
+      <button onClick={toggleIsModalOpen}>Edit</button>
+
+    </div >
   )
 }
