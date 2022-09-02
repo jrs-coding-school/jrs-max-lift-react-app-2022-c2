396547b6e466efec4179e1cd6bd8b1dca1d6f448
Eric <charlesericvaughn@gmail.com>
2022-09-02 10:20:30 -0400

    added icons to nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook, faDumbbell, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 export default function Nav() {
+
   return (
-
-    // <i class="fa-solid fa-house"></i>
-
     <nav>
       <div class="left">
-        <Link to="/">Home</Link>
-        <Link to="/progress">Progress</Link>
-        <Link to="/history">History</Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faHouse} />
+        </Link>
+        <Link to="/progress"> 1 RMs
+          <FontAwesomeIcon icon={faDumbbell} />
+        </Link>
+        <Link to="/history"> History
+          <FontAwesomeIcon icon={faBook} />
+        </Link>
       </div>
 
       <div class="right">
